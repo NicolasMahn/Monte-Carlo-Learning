18339f2672b9522146b66b347ea37518f943507c
nicolas <nicolas.mahn@gmx.de>
2022-12-27 22:19:17 +0100

    final steps

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
-import random
-
+import numpy.random
 import numpy as np
 
+random = numpy.random.random
 
-def monte_carlo_simple():
+
+def very_simple_monte_carlo():
 
     rounds = list()
     for episode in range(0, 1000000):  # 1 million episodes are played
@@ -12,55 +13,115 @@ def monte_carlo_simple():
         round_ = 0
         euro = 4
         while euro >= 2:   # The number of states differs in every episode
-            euro -= 2
             round_ += 1
-            if random.random() < p:
-                p -= 0.1 * random.random()
+            if random() < p:
+                euro -= 2
+                p -= 0.1 * random()
                 euro += 3
         rounds.append(round_)
     return sum(rounds)/len(rounds)  # >> 20.511079
 
 
-def monte_carlo_learning():
-    return_ = dict()  # all returns collected in an episode
+def simple_monte_carlo_learning():
+    avg_fr = np.full([30], 0, dtype=float)  # avg_fr starting with 0s
     n = np.full([30], 0)  # number of times a state has been tread upon
-    v = np.full([30], 0, dtype=float)  # V starting with the random number 0
-    for episode in range(0, 1000000):  # 1 million episodes are played
+    for episode in range(1, 10001):  # 10 000 episodes are played
+
+        fr = dict()
         p = 1
-        round_ = 0  # state
+        state = 0  # current round
         euro = 4  # reward
-        while euro >= 2 and round_ < 29:
+        # random policy
+        # can't play when gambler doesn't have enough money
+        while state < 30:
+
             euro -= 2
-            round_ += 1
-            if random.random() < p:
-                p -= 0.1 * random.random()
+            if random() < p:
+                p -= 0.1 * random()
                 euro += 3
-            return_[round_] = euro
+            fr[state] = euro
+            n[state] += 1
+            state += 1
+
+        for state in fr:
+            avg_fr[state] = avg_fr[state] + \
+                            (1 / n[state]) * (fr[state] - avg_fr[state])
+    return avg_fr  # argmax(avg_euro) >> 7
+
+
+def simple_monte_carlo_learning_plot():
+    avg_fr_list = list()
+
+    avg_fr = np.full([30], 0, dtype=float)  # avg_fr starting with 0s
+    n = np.full([30], 0)  # number of times a round_ has been tread upon
+    for episode in range(1, 10001):  # 10 000 episodes are played
+
+        fr = dict()
+
+        p = 1
+        round_ = 0  # current round
+        euro = 4  # reward
+        # random policy
+        # can't play when gambler doesn't have enough money
+        while round_ < 30:
+
+            euro -= 2
+            if random() < p:
+                p -= 0.1 * random()
+                euro += 3
+            fr[round_] = euro
             n[round_] += 1
+            round_ += 1
 
-        for round_ in return_:
-            v[round_] = v[round_] + (1 / n[round_]) * (return_[round_] - v[round_])
-    return v  # argmax(v) >> 10
+        for round_ in fr:
+            avg_fr[round_] = avg_fr[round_] + (1 / n[round_]) * \
+                                           (fr[round_] - avg_fr[round_])
+
+        if episode in [1, 10, 100, 1000, 10000]:
+            avg_fr_list.append(avg_fr.copy())
+    return avg_fr_list  # argmax(avg_fr) >> 7
 
 
-def plot_v(v):
-    plt.title("Value of each State")
-    plt.xlabel("round | state")
-    plt.ylabel("average return")
+def plot_avg_euro(v):
+    plt.title("Average Euro per Round")
+    plt.xlabel("round")
+    plt.ylabel("expected average return on investment (€)")
     plt.xlim([0, 30])
     plt.ylim([0, 9])
-    plt.plot(v, color="#008854")
+    plt.plot(v, color="#008855", linewidth=3)
+    plt.show()
+
+
+def plot_avg_euro_list(v_list, v_list_episode_count, legend=True):
+    plt.title("Average Euro per Round")
+    plt.xlabel("at round")
+    plt.ylabel("expected average return on investment (€)")
+
+    colors = ["#880033", "#550088", "#885500", "#007788",  "#008855", "#003388",
+              "#338800", "#880077", "#881100", "#778800"]
+
+    for j in range(len(v_list)):
+        episode = v_list_episode_count[j]
+        if j == len(v_list_episode_count):
+            color = "#008854"
+        else:
+            color = colors[j]
+        plt.plot(v_list[j], color=color, label=f"episode {episode}", linewidth=3)
+    if legend:
+        plt.legend()
     plt.show()
 
 
 def main():
 
-    v = monte_carlo_learning()
+    print("")
 
-    print(np.argmax(v))
-    print(v)
+    avg_fr_list = simple_monte_carlo_learning_plot()
 
-    plot_v(v)
+    print(np.argmax(avg_fr_list[-1]))
+    print(avg_fr_list[-1])
+
+    plot_avg_euro_list(avg_fr_list, [1, 10, 100, 1000, 10000], legend=True)
 
 
 
